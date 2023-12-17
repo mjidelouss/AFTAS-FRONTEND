ec8e43a9aaf3ce3afe2455f8df9988c69bc6fc51
mjidelouss <mjid.elouss@gmail.com>
2023-12-17 13:05:06 +0100

    Hunt logged in Successfully

--- src/app/component/hunt/hunt.component.ts
+++ src/app/component/hunt/hunt.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HuntService } from 'src/app/service/hunt.service';
 
 @Component({
   selector: 'app-hunt',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HuntComponent {
 
+  constructor(private huntService: HuntService) {}
+
+  addHunt(hunt: any) {
+    console.log(hunt)
+    this.huntService.addHunt(hunt).subscribe(
+      (response) => {
+        console.log('Hunt added successfully', response);
+      },
+      (error) => {
+        console.error('Error adding Hunt', error);
+      }
+    );
+  }
 }
--- src/app/service/hunt.service.ts
+++ src/app/service/hunt.service.ts
@@ -7,9 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class HuntService {
   private apiUrl = "http://127.0.0.1:8080/api/hunting"
+
   constructor(private http:HttpClient) {}
 
   getHunts(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
+
+  addHunt(hunt: any): Observable<any> {
+    return this.http.post(this.apiUrl, hunt);
+  }
+
 }
